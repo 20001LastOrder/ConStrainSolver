a042ae02df8e6f819797ee128de2ff96e0123b63
Boqi Chen <boqi.chen@mail.mcgill.ca>
2024-10-09 22:52:11 -0400

    Add LLM for string constraint solving

---
+++ llm_string/utils.py
@@ -0,0 +1,16 @@
+import json
+import re
+
+from langchain_core.output_parsers import PydanticOutputParser
+
+
+class REPydanticOutputParser(PydanticOutputParser):
+    def parse(self, output: str):
+        json_pattern = re.compile(r"```json\n(.*?)\n```", re.DOTALL)
+        match = json_pattern.search(output)
+
+        if match:
+            json_object = json.loads(match.group(1))
+            return self._parse_obj(json_object)
+        else:
+            raise ValueError("No JSON found in the output.")
